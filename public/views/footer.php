14ee2a8c90322b89d3015382a033e57cca8d4a35
Mehedi-Ahmed <96369928+Mehedi-Ahmed@users.noreply.github.com>
2025-05-28 23:04:25 +0600

    Update footer.php

--- public/views/footer.php
+++ public/views/footer.php
@@ -1,7 +1,3 @@
-<!-- <footer class="text-center py-3 border-top">
-    <p class="mb-0">A simple vehicle listing app by Mehedi</p>
-</footer> -->
-
 <footer class="text-center mt-4 py-3 text-muted small">
         Ostad Module 3 Project 1 by Mehedi
     </footer>
